feea54445268f4af85a404a43f2efa9c7b6b4f3e
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2021-11-06 13:17:23 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MaterialApp(home: MainScreen()));
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Book', amount: 12.34, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Breakfast', amount: 50.4, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('E-Planner')),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            width: double.infinity,
             child: Card(
               child: Text('Chart 1'),
               color: Colors.grey,
             ),
           ),
-          Card(
-            child: Text('Chart 2')
-          ),
-          Card(
-            child: Text('List of transactions')
-          )
+          Card(color: Colors.lightBlue, child: Text('Chart 2')),
+          Card(color: Colors.orange, child: Text('List of transactions'))
         ],
       ),
     );

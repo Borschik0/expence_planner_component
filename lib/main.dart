70c6ecb2d5c06217b54865aec4e2f8b2e4d8c0aa
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2021-11-09 23:45:17 +0200

    9

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
-import './widgets/user_transactions.dart';
+import 'models/transaction.dart';
+import 'widgets/new_transactions.dart';
+import 'widgets/transaction_list.dart';
 
 void main() => runApp(MaterialApp(home: MainScreen()));
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New Book', amount: 12.34, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Breakfast', amount: 50.4, date: DateTime.now())
+  ];
+
+  void _addnewTransactions(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addnewTransactions),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('E-Planner')),
+      appBar: AppBar(
+        title: Text('E-Planner'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                return _startAddNewTransaction(context);
+              },
+              icon: Icon(Icons.add))
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -20,10 +66,17 @@ class MainScreen extends StatelessWidget {
               ),
             ),
             Card(color: Colors.lightBlue, child: Text('Chart 2')),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          return _startAddNewTransaction(context);
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
